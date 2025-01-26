b9887cfb68a4e7f14a909dadcb1720c630543227
Mike Valeriano <mkvlrn@proton.me>
2025-01-26 17:02:41 -0300

    ci: adjust precommit hook

--- src/modules/logger.ts
+++ src/modules/logger.ts
@@ -1,19 +1,16 @@
-import { pino, type LoggerOptions } from "pino";
+import { pino } from "pino";
 import { ENV } from "~/modules/environment";
-
-const transport: LoggerOptions["transport"] = {
-  target: ENV.devMode ? "pino-pretty" : "@logtail/pino",
-  options: ENV.devMode
-    ? {
-        colorize: ENV.devMode,
-        ignore: ENV.devMode ? "pid,hostname" : "",
-        translateTime: "yyyy-mm-dd hh:MM:ss TT",
-        destination: ENV.devMode ? "" : "./logs/error.log",
-      }
-    : { sourceToken: ENV.logtailToken },
-};
 
 export const logger = pino({
   level: ENV.logLevel as pino.Level,
-  transport,
+  transport: {
+    target: ENV.devMode ? "pino-pretty" : "@logtail/pino",
+    options: ENV.devMode
+      ? {
+          colorize: true,
+          ignore: "pid,hostname",
+          translateTime: "yyyy-mm-dd hh:MM:ss TT",
+        }
+      : { sourceToken: ENV.logtailToken },
+  },
 });

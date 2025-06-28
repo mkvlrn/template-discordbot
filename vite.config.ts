98253a4513d00f5693c485a1f42e71c818576661
Mike Valeriano <mkvlrn@proton.me>
2025-06-28 06:21:25 -0300

    chore: adjust vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,15 @@ import { defineConfig } from "vitest/config";
 // application entry point
 const entry = "./src/main.ts";
 // dependencies to be internalized/bundled
-const internalPackages: string[] = [];
+const internalPackages: (string | RegExp)[] = [];
+// dependencies to be externalized forcefully
+const externalPackages: (string | RegExp)[] = [];
 
 export default defineConfig({
   plugins: [
     // externalize node builtins
     nodeExternals({
+      include: externalPackages,
       exclude: internalPackages,
     }),
     // resolve tsconfig path aliases

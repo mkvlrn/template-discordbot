7fbbd701b7c57b3c7d2619dd5a9831e12bc91f9c
Mike Valeriano <mkvlrn@proton.me>
2025-08-02 15:50:29 -0300

    chore: update registration script

--- scripts/register-commands.ts
+++ scripts/register-commands.ts
@@ -1,44 +1,51 @@
 /** biome-ignore-all lint/correctness/noNodejsModules: needed */
 import process from "node:process";
 import { type Guild, REST, type RequestData, Routes } from "discord.js";
-import type { Logger } from "pino";
 import { getCommands } from "#/modules/command";
 import { env } from "#/modules/env";
 import { getLogger } from "#/modules/logger";
 
-function getServerId(logger: Logger): string {
-  const errorMessage = "Invalid arguments. Expected: 'register <serverId>' or 'register global'";
+const logger = getLogger();
 
-  // errors out if there's no args - must be exactly one
+function getArgs(): { serverId: string; unregister: boolean } {
+  let errorMessage = "Invalid arguments:\n";
+  errorMessage += " Expected: '<serverId>' or '<serverId> --unregister'.\n";
+  errorMessage += " <serverId> must be a valid discord server id or 'global'";
+
   const args = process.argv.slice(2);
-  if (args.length !== 1 || args[0] === undefined) {
+  if (args.length === 0 || args.length > 2) {
     logger.error(errorMessage);
     process.exit(1);
   }
 
-  // errors out if the arg is not "global" or is an empty string
-  const serverId = args[0].trim();
-  if (serverId !== "global" && serverId === "") {
+  const serverId = args[0]?.trim();
+  if (!serverId) {
     logger.error(errorMessage);
     process.exit(1);
   }
 
-  return serverId;
+  const unregister = args.length === 2 && args[1] === "--unregister";
+  if (args.length === 2 && !unregister) {
+    logger.error(errorMessage);
+    process.exit(1);
+  }
+
+  return { serverId, unregister };
 }
 
-async function getTarget(logger: Logger, serverId: string, restClient: REST): Promise<string> {
+async function getTarget(serverId: string, restClient: REST): Promise<string> {
   let target = "globally";
   if (serverId !== "global") {
     try {
       const guild = (await restClient.get(Routes.guild(serverId))) as Guild;
-      target = `server '${guild.name}' (${serverId})`;
+      target = `on server '${guild.name}' (${serverId})`;
     } catch (error) {
       logger.error((error as Error).message);
       process.exit(1);
     }
   }
 
-  logger.info(`Registering commands to ${target}`);
+  logger.info(`Updating commands ${target}`);
 
   return target;
 }
@@ -46,21 +53,23 @@ async function getTarget(logger: Logger, serverId: string, restClient: REST): Pr
 async function registerCommands(): Promise<void> {
   const logger = getLogger();
   const commands = await getCommands();
-  const serverId = getServerId(logger);
+  const { serverId, unregister } = getArgs();
   const restClient = new REST().setToken(env("botToken"));
-  const target = await getTarget(logger, serverId, restClient);
+  const target = await getTarget(serverId, restClient);
 
   const route =
     serverId === "global"
       ? Routes.applicationCommands(env("botClientId"))
       : Routes.applicationGuildCommands(env("botClientId"), serverId);
   const payload: RequestData = {
-    body: Array.from(commands, ([_, command]) => command.data.toJSON()),
+    body: unregister ? [] : Array.from(commands, ([_, command]) => command.data.toJSON()),
   };
 
   try {
     await restClient.put(route, payload);
-    logger.info(`Commands registered ${target}: ${[...commands.keys()].join(", ")}`);
+    logger.info(
+      `Commands ${unregister ? "un" : ""}registered ${target}: ${[...commands.keys()].join(", ")}`,
+    );
   } catch (error) {
     logger.error((error as Error).message);
     throw error;

a1e4010336151fc81fd2929e1d194907dce8629a
Mike Valeriano <186238078+mkvlrn@users.noreply.github.com>
2025-08-15 06:19:07 -0300

    chore: bump deps

--- src/modules/command.ts
+++ src/modules/command.ts
@@ -4,45 +4,51 @@ import { dirname, join } from "node:path";
 import { fileURLToPath, pathToFileURL } from "node:url";
 import { type CommandInteraction, SlashCommandBuilder } from "discord.js";
 
-export type Command = {
+type Command = {
   data: SlashCommandBuilder;
   execute: (interaction: CommandInteraction) => Promise<void>;
 };
 
+const MODULES_DIR = dirname(fileURLToPath(import.meta.url));
+const COMMANDS_DIR = join(MODULES_DIR, "../commands");
+const FILE_EXTENSION_REGEX = /.(t|j)s/;
+
 let commands: Map<string, Command>;
 
-export function createCommand(
-  name: string,
-  description: string,
-  executable: Command["execute"],
-): Command {
+function createCommand(name: string, description: string, executable: Command["execute"]): Command {
   return {
     data: new SlashCommandBuilder().setName(name).setDescription(description),
     execute: executable,
   };
 }
 
-const fileExtensionRegex = /.(t|j)s/;
+async function loadCommandFromFile(file: string): Promise<Command | undefined> {
+  const commandName = file.replace(FILE_EXTENSION_REGEX, "");
+  const commandPath = pathToFileURL(join(COMMANDS_DIR, file));
+  const commandModule = await import(commandPath.href);
+  const command = commandModule[commandName] as Command;
 
-export async function getCommands(): Promise<Map<string, Command>> {
+  if (
+    command &&
+    command.data instanceof SlashCommandBuilder &&
+    typeof command.execute === "function"
+  ) {
+    return command as Command;
+  }
+
+  return undefined;
+}
+
+async function getCommands(): Promise<Map<string, Command>> {
   if (!commands) {
     commands = new Map<string, Command>();
 
-    const modulesDir = dirname(fileURLToPath(import.meta.url));
-    const commandsDir = join(modulesDir, "../commands");
-    const files = await readdir(commandsDir);
+    const files = await readdir(COMMANDS_DIR);
+    const importPromises = files.filter((f) => !f.includes("map")).map(loadCommandFromFile);
+    const importedCommands = await Promise.all(importPromises);
 
-    for (const file of files.filter((f) => !f.includes("map"))) {
-      const commandName = file.replace(fileExtensionRegex, "");
-      const commandPath = pathToFileURL(join(commandsDir, file));
-      const commandModule = await import(commandPath.href);
-      const command = commandModule[commandName] as Command;
-
-      if (
-        command &&
-        command.data instanceof SlashCommandBuilder &&
-        typeof command.execute === "function"
-      ) {
+    for (const command of importedCommands) {
+      if (command) {
         commands.set(command.data.name, command);
       }
     }
@@ -50,3 +56,5 @@ export async function getCommands(): Promise<Map<string, Command>> {
 
   return commands;
 }
+
+export { type Command, createCommand, getCommands };

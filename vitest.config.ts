42ce4209a7544edf567ea8c11a6505905b209937
mkvlrn <mkvlrn@gmail.com>
2026-08-07 09:45:30 -0300

    chore: bump deps

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,23 +1,4 @@
-import { defineConfig } from "vitest/config";
+import baseConfig from "@mkvlrn/config/vitest";
+import { defineConfig, mergeConfig } from "vitest/config";
 
-export default defineConfig(() => {
-  return {
-    test: {
-      include: ["**/**/*.{test,spec}.ts"],
-      exclude: ["node_modules"],
-      watch: false,
-      coverage: {
-        all: true,
-        clean: true,
-        cleanOnRerun: true,
-        include: ["src"],
-        exclude: ["**/*.{test,spec}.ts", "src/main.ts"],
-      },
-      // biome-ignore lint/style/useNamingConvention: needed for vitest
-      env: { NODE_ENV: "test" },
-      environment: "node",
-      passWithNoTests: true,
-      setupFiles: [],
-    },
-  };
-});
+export default defineConfig((configEnv) => mergeConfig(baseConfig(configEnv), defineConfig({})));
